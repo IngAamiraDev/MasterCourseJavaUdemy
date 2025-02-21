164e9ea283994829fbd11125a7064936ff55c603
ingaamira <andres.mira@outlook.com>
2025-02-21 11:00:04 -0500

    Update docs

---
+++ src/main/java/com/ingaamira/modules/_24/_collections/set/EjemploHashSetAgregar.java
@@ -0,0 +1,33 @@
+package com.ingaamira.modules._24._collections.set;
+
+import java.util.*;
+
+public class EjemploHashSetAgregar {
+    public static void main(String[] args) {
+
+        Set<String> hs = new HashSet<>();
+        System.out.println(hs.add("uno"));
+        System.out.println(hs.add("dos"));
+        System.out.println(hs.add("tres"));
+        System.out.println(hs.add("cuatro"));
+        System.out.println(hs.add("cinco"));
+        System.out.println(hs);
+
+        /*Ordenando ArrayList a partir del Set*/
+        List<String> lista = new ArrayList<>(hs);
+        Collections.sort(lista);
+        System.out.println(lista);
+
+        boolean b = hs.add("tres");
+        System.out.println("permite elementos duplicados = " + b);
+        System.out.println(hs);
+
+        /* Set:
+         - No permite elementos duplicados
+         - No mantiene un orden específico al almacenar los datos
+         - Cada add retorna un boolean
+         - Los "Set" como las "Listas" comparten la misma interface "collection"
+        */
+
+    }
+}
